4cd43cee26e2c419f333be1e07922a11172dcc58
DaNaY111 <danaybroths@gmail.com>
2025-05-23 13:40:55 +0300

    fix: prod api fix

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,11 +1,10 @@
 import { HandleCheckoutSessionCompleted } from "@/lib/stripe/handleCheckoutSessionCompleted";
 import { stripe } from "@/lib/stripe/stripe";
-import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const body = await request.text();
-  const signature = headers().get("stripe-signature") as string;
+  const signature = request.headers.get("stripe-signature") as string;
 
   try {
     const event = stripe.webhooks.constructEvent(
